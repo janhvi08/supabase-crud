54909b8afb0ed95f424e523b15cfe61c2a036fca
Janhvi <Janhvi2013031@akgec.ac.in>
2023-07-07 17:53:00 +0530

    my-first-supabase-crud

---
+++ src/createClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+
+
+export const supabase= createClient(
+    "https://qwvsvkupdtdkjdlbhsdk.supabase.co", 
+     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3dnN2a3VwZHRka2pkbGJoc2RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg3MTUyNDgsImV4cCI6MjAwNDI5MTI0OH0.QtmY6qo9IpkPz47n_R1yXn3H73lfCHHmExeiVfa8ZHw"
+      )
